4e1071681108a3369d5589b30c448e3c2fb21897
Mahdi Abedi <abedi@jeyserver.com>
2016-10-21 13:27:45 +0330

    compatibility to php 5.x

--- libraries/users/authorization.php
+++ libraries/users/authorization.php
@@ -1,7 +1,6 @@
 <?php
 namespace packages\financial;
 use \packages\userpanel\authorization as UserPanelAuthorization;
-use \packages\userpanel\authentication;
 class authorization extends UserPanelAuthorization{
 	static function is_accessed($permission, $prefix = 'financial'){
 		return parent::is_accessed($permission, $prefix);
