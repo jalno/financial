b7cd5b3d2d7d7035270ae6f3c0b05d1d7f7fc768
Mahdi Abedi <abedi@jeyserver.com>
2016-11-04 17:32:24 +0330

    define permissions

---
+++ listeners/settings/usertype.php
@@ -0,0 +1,21 @@
+<?php
+namespace packages\financial\listeners\settings;
+use \packages\userpanel\usertype\permissions;
+class usertype{
+	public function permissions_list(){
+		$permissions = array(
+			'transactions_list',
+			'transactions_add',
+			'transactions_edit',
+			'transactions_delete',
+
+			'transactions_pays_accept',
+			'transactions_pay_delete',
+			'transactions_product_delete'
+
+		);
+		foreach($permissions as $permission){
+			permissions::add('financial_'.$permission);
+		}
+	}
+}
