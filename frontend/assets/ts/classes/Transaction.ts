41444b3ec7bc585cd3dab51d19c1115b9ea9bce1
Mahdi Abedi <abedi@jeyserver.com>
2017-06-17 15:09:28 +0430

    edit transaction

--- frontend/assets/ts/classes/Transaction.ts
+++ frontend/assets/ts/classes/Transaction.ts
@@ -1,9 +1,11 @@
 import List from "./Transaction/List";
 import Add from "./Transaction/Add";
+import Edit from "./Transaction/Edit";
 export default class Transaction{
 	public static initIfNeeded(){
 		List.initIfNeeded();
 		Add.initIfNeeded();
+		Edit.initIfNeeded();
 	}
 	
 }
