0eab4305c76aa1f8aa4429f00d084a36eaff9e95
Mahdi Abedi <abedi@jeyserver.com>
2018-10-26 10:13:34 +0330

    set dashboard user credit shortcut

--- frontend/listeners/clipone/dashboard.php
+++ frontend/listeners/clipone/dashboard.php
@@ -1,24 +1,53 @@
 <?php
 namespace themes\clipone\listeners\financial;
-use \packages\base;
-use \packages\base\translator;
-use \packages\userpanel;
-use \packages\financial\authorization;
-use \themes\clipone\views\dashboard as view;
-use \themes\clipone\views\dashboard\shortcut;
-class dashboard{
+use packages\base\{translator, db};
+use packages\userpanel;
+use packages\financial\{authorization, authentication, currency, transaction};
+use themes\clipone\views\dashboard as view;
+use themes\clipone\views\dashboard\shortcut;
+
+class dashboard {
 	public function initialize(){
 		$this->addShortcuts();
 	}
 	protected function addShortcuts(){
-		if(authorization::is_accessed('transactions_list')){
+		$user = authentication::getUser();
+		if(authorization::is_accessed("transactions_list")){
+			$types = authorization::childrenTypes();
+			$transaction = new transaction();
+			db::join("userpanel_users", "userpanel_users.id=financial_transactions.user", "INNER");
+			if ($types) {
+				$transaction->where("userpanel_users.type", $types, "in");
+			} else {
+				$transaction->where("userpanel_users.id", $user->id);
+			}
+			$transaction->where("financial_transactions.status", transaction::unpaid);
+			$transactions = $transaction->count();
 			$shortcut = new shortcut("transactions");
-			$shortcut->icon = 'fa fa-money';
-			$shortcut->color = shortcut::bricky;
-			$shortcut->title = translator::trans('shortcut.transactions.title');
-			$shortcut->text = translator::trans('shortcut.transactions.text');
-			$shortcut->setLink(translator::trans('shortcut.transactions.link'), userpanel\url('transactions'));
+			$shortcut->icon = "fa fa-money";
+			if ($transactions) {
+				$shortcut->title = $transactions;
+				$shortcut->text = translator::trans("shortcut.transactions.unpaid.transaction");
+				$shortcut->setLink(translator::trans("shortcut.transactions.link"), userpanel\url("transactions"));
+			} else {
+				$shortcut->text = translator::trans("shortcut.transactions.unpaid.transaction.iszere");
+				if (authorization::is_accessed("transactions_addingcredit")) {
+					$shortcut->setLink(translator::trans("transaction.adding_credit"), userpanel\url("transactions/addingcredit"));
+				}
+			}
 			view::addShortcut($shortcut);
 		}
+		$shortcut = new shortcut("transactions.user.credit");
+		$shortcut->icon = "fa fa-credit-card-alt";
+		if ($user->credit > 0) {
+			$shortcut->title = number_format($user->credit);
+			$shortcut->text = currency::getDefault($user)->title . " " . translator::trans("shortcut.transactions.user.credit");
+		} else {
+			$shortcut->text = translator::trans("shortcut.transactions.user.credit.iszero");
+		}
+		if (authorization::is_accessed("transactions_addingcredit")) {
+			$shortcut->setLink(translator::trans("transaction.adding_credit"), userpanel\url("transactions/addingcredit"));
+		}
+		view::addShortcut($shortcut);
 	}
 }
