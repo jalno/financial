3bd3041b679beff5db9922149eab0d615bbf12f5
Hossein Hosni <hosni.hossein@gmail.com>
2020-08-18 15:42:54 +0430

    #67 Remove unused col-xs-12 in onlinepay

--- frontend/html/transactions/pay/onlinepay.php
+++ frontend/html/transactions/pay/onlinepay.php
@@ -1,65 +1,62 @@
 <?php
-use \packages\userpanel;
-use \packages\base\{translator, http};
-use \themes\clipone\utility;
-use packages\financial\authentication;
+use packages\base\Http;
+use packages\financial\Authentication;
+use packages\userpanel;
+use themes\clipone\Utility;
+
 $parameter = array();
-if ($token = http::getURIData("token")) {
+$token = Http::getURIData("token");
+if ($token) {
 	$parameter["token"] = $token;
 }
-$isLogin = authentication::check();
+$isLogin = Authentication::check();
+
 $this->the_header(!$isLogin ? "logedout" : "");
 ?>
-<!-- start: PAGE CONTENT -->
 <div class="row">
 	<div class="<?php echo !$isLogin ? "col-sm-6 col-sm-offset-3 col-xs-12" : "col-sm-8 col-sm-offset-2"; ?>">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<i class="clip-phone-3"></i> <?php echo translator::trans('pay.online.select'); ?>
+				<i class="clip-phone-3"></i> <?php echo t('pay.online.select'); ?>
 				<div class="panel-tools">
 					<a class="btn btn-xs btn-link panel-collapse collapses" href="#"></a>
 				</div>
 			</div>
 			<div class="panel-body panel-online-pay-body">
 				<form class="online-pay-form" action="<?php echo userpanel\url('transactions/pay/onlinepay/'.$this->transaction->id, $parameter); ?>" method="POST" role="form">
-					<?php $this->createField([
-						"name" => "currency",
-						"type" => "hidden",
-					]); ?>
-					<div class="row">
-						<div class="col-xs-12">
-							<?php
-							$this->createField(array(
-								'type' => 'select',
-								'name' => 'payport',
-								'label' => translator::trans("pay.online.payport"),
-								'options' => $this->getPayportsForSelect()
-							));
-							?>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-xs-12">
-							<?php
-							$this->createField(array(
-								'name' => 'price',
-								'ltr' => true,
-								'label' => translator::trans("pay.price"),
-								"input-group" => [
-									"right" => [
-										[
-											"type" => "addon",
-											"text" => $this->transaction->currency->title,
-										]
-									]
-								]
-							));
-							?>
-						</div>
-					</div>
+					<?php
+					$fields = array(
+						array(
+							'name' => 'currency',
+							'type' => 'hidden',
+						),
+						array(
+							'type' => 'select',
+							'name' => 'payport',
+							'label' => t('pay.online.payport'),
+							'options' => $this->getPayportsForSelect(),
+						),
+						array(
+							'name' => 'price',
+							'ltr' => true,
+							'label' => t('pay.price'),
+							'input-group' => array(
+								'right' => array(
+									array(
+										'type' => 'addon',
+										'text' => $this->transaction->currency->title,
+									),
+								),
+							),
+						),
+					);
+					foreach ($fields as $field) {
+						$this->createField($field);
+					}
+					?>
 					<div class="row" style="margin-top: 20px;margin-bottom: 20px;">
 						<div class="col-md-offset-4 col-md-4">
-							<button class="btn btn-teal btn-block" type="submit"><i class="fa fa-arrow-circle-left"></i> <?php echo translator::trans('pay'); ?></button>
+							<button class="btn btn-teal btn-block" type="submit"><i class="fa fa-arrow-circle-left"></i> <?php echo t('pay'); ?></button>
 						</div>
 					</div>
 				</form>
